ac0d6b54671c9e6a236140de62e761a107e938ad
Mariana Gonçalves Feitosa <feitosamarigoncalves@gmail.com>
2024-11-03 18:15:33 -0300

    media
    mudanças no media queries

--- MeuSite/JS/script.js
+++ MeuSite/JS/script.js
@@ -1,3 +1,13 @@
-function alertaHello(){
-    alert("Olá Pessoal!")
+
+
+function realizarPesquisa() {
+    const campoPesquisa = document.getElementById("campoPesquisa");
+    const valorPesquisa = campoPesquisa.value;
+    const resultado = document.getElementById("resultado");
+
+    if (valorPesquisa) {
+        resultado.textContent = `Você pesquisou por: "${valorPesquisa}"`;
+    } else {
+        resultado.textContent = "Por favor, digite algo para pesquisar.";
+    }
 }
